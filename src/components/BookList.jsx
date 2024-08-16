e1cc31a9a5ed4bc68fa2bc3c1eb2cde7fc55512c
Shariar Duke <shariarmahmud225@gmail.com>
2024-08-17 05:39:38 +0600

    BookList.jsx left side of the page

--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -3,7 +3,7 @@ import Book from "./Book";
 export default function BookList() {
   const [filterOption, setFilterOption] = useState("all");
   const handleFilter = (option) => {
-    setFilterOption(option)
+    setFilterOption(option);
   };
   return (
     <div>
@@ -12,25 +12,42 @@ export default function BookList() {
         <div className="flex gap-[10px]">
           <button
             onClick={() => handleFilter("all")}
-            className={`${filterOption === "all" ? "bg-[#17B169] text-white" : "bg-white text-gray-700"}  border-[1px] border-[#17B169]  rounded-full px-[24px] py-[4px] text-[14px] font-bold `}
+            className={`${
+              filterOption === "all"
+                ? "bg-[#17B169] text-white"
+                : "bg-white text-gray-700"
+            }  border-[1px] border-[#17B169]  rounded-full px-[24px] py-[4px] text-[14px] font-bold `}
           >
             All
           </button>
           <button
             onClick={() => handleFilter("featured")}
-            className={`${filterOption === "featured" ? "bg-[#17B169] text-white" : "bg-white text-gray-700"} border-[1px] border-[#17B169] rounded-full px-[24px] py-[4px] text-[14px] font-bold `}
+            className={`${
+              filterOption === "featured"
+                ? "bg-[#17B169] text-white"
+                : "bg-white text-gray-700"
+            } border-[1px] border-[#17B169] rounded-full px-[24px] py-[4px] text-[14px] font-bold `}
           >
             Featured
           </button>
         </div>
       </div>
-      <div className="max-h-[85vh] overflow-y-auto mt-[30px] grid grid-cols-2 gap-[20px]  pr-[10px]">
-        <Book/>
-        <Book/>
-        <Book/>
-    
-   
-      
+
+      <div className="pb-[28px] max-h-[80vh] overflow-y-auto">
+        <div className="grid grid-cols-2 gap-[20px] pr-[10px] py-[20px]">
+          <Book />
+          <Book />
+          <Book />
+          <Book />
+          <Book />
+          <Book />
+          <Book />
+          <Book />
+          <Book />
+          <Book />
+          <Book />
+          <Book />
+        </div>
       </div>
     </div>
   );
