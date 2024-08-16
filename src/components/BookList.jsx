f5df25b120cde529fa3f7e93470560f70526a927
Shariar Duke <shariarmahmud225@gmail.com>
2024-08-16 23:13:16 +0600

    Upper part of BookList

---
+++ src/components/BookList.jsx
@@ -0,0 +1,31 @@
+import { useState } from "react";
+export default function BookList() {
+  const [filterOption, setFilterOption] = useState("all");
+  const handleFilter = (option) => {
+    setFilterOption(option)
+  };
+  return (
+    <div>
+      <div className="flex justify-between">
+        <p className="font-bold">Book List</p>
+        <div className="flex gap-[10px]">
+          <button
+            onClick={() => handleFilter("all")}
+            className={`${filterOption === "all" ? "bg-[#17B169] text-white" : "bg-white text-gray-700"}  border-[1px] border-[#17B169]  rounded-full px-[24px] py-[4px] text-[14px] font-bold `}
+          >
+            All
+          </button>
+          <button
+            onClick={() => handleFilter("featured")}
+            className={`${filterOption === "featured" ? "bg-[#17B169] text-white" : "bg-white text-gray-700"} border-[1px] border-[#17B169] rounded-full px-[24px] py-[4px] text-[14px] font-bold `}
+          >
+            Featured
+          </button>
+        </div>
+      </div>
+      <div className="max-h-[70vh] overflow-auto">
+        <p>BookItem</p>
+      </div>
+    </div>
+  );
+}
