2ac25323c7a2024c782b1775883aa4f784329d1b
Shariar Duke <shariarmahmud225@gmail.com>
2024-08-19 00:19:27 +0600

    providing the store

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
+import { Provider } from "react-redux";
 import AddBook from "./components/AddBook";
 import BookList from "./components/BookList";
 import Header from "./components/Header";
+import store from "./redux/store";
 export default function App() {
   return (
-    <div className="h-[100vh] bg-[#F8F8F8] px-[20px]">
+    <Provider store={store}>
+  <div className="h-[100vh] bg-[#F8F8F8] px-[20px]">
       <Header/>
       <div className="grid grid-cols-4 gap-[50px] pt-[20px]">
         <div className="col-span-3">
@@ -17,5 +20,7 @@ export default function App() {
         </div>
       </div>
     </div>
+    </Provider>
+  
   );
 }
