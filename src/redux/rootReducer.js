1880418612d0fbc24995a7556748b30989a1ed03
Shariar Duke <shariarmahmud225@gmail.com>
2024-08-19 15:20:00 +0600

    search and Filter added in RootReducer.js

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import bookReducer from "./books/reducer";
+import filterReducer from "./searchAndFilter/reducer";
 
 const rootReducer = combineReducers({
-    books : bookReducer
+    books : bookReducer,
+    filters : filterReducer
 })
 
 
