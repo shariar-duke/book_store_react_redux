25c8994fb66533948d3ebcf3e3bcda6f8d294824
Shariar Duke <shariarmahmud225@gmail.com>
2024-08-19 08:21:14 +0600

    reducer.js

--- src/redux/books/reducer.js
+++ src/redux/books/reducer.js
@@ -1,15 +1,22 @@
-import { LOADED } from "./actionTypes";
+import { ADDED, LOADED } from "./actionTypes";
 import { initialState } from "./initialState";
 
 function nextBookId(books) {
-  const maxId = books.reduce((maxId, book) => Math.max(maxId, book.id), -1);
-  return maxId + 1;
-}
+    const maxId = books.reduce((maxId, book) => Math.max(maxId, book.id), -1);
+    return maxId + 1;
+  }
+
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case LOADED:
       return action.payload;
+
+    case ADDED: 
+      return [...state, {
+        ...action.payload,
+        id: nextBookId(state)
+      }]
 
     default:
       return state;
