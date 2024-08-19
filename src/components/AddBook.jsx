a5eafb19d9c044e1c3cdee89da1563baa74c6bb6
Shariar Duke <shariarmahmud225@gmail.com>
2024-08-19 10:51:55 +0600

    addBook.jsx

--- src/components/AddBook.jsx
+++ src/components/AddBook.jsx
@@ -1,18 +1,38 @@
-import { useState } from "react";
+/* eslint-disable react/prop-types */
+
+import { useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
 import addBook from "../redux/books/thunk/addBook";
-export default function AddBook() {
-  const [state, setState] = useState({
+import updateBook from "../redux/books/thunk/editBook";
+export default function AddBook({ editBook, setEditBook }) {
+  const initialState = {
     name: "",
     author: "",
     thumbnail: "",
     price: "",
     rating: 0,
     featured: false,
-  });
+  };
+  console.log("The edit book is", editBook);
+  const [state, setState] = useState(editBook || initialState);
+  const [isEditing, setIsEditing] = useState(false);
+
+
+
+  useEffect(() => {
+    if (editBook && Object.keys(editBook).length > 0) {
+      setState(editBook);
+      setIsEditing(true);
+    } else {
+      setState(initialState);
+      setIsEditing(false);
+    }
+  }, [editBook]);
+
+  console.log("The state for editing mode is", state);
 
   const { name, author, thumbnail, price, rating, featured } = state;
-    const dispatch = useDispatch()
+  const dispatch = useDispatch();
 
   const handleChange = (e) => {
     const name = e.target.name;
@@ -22,23 +42,26 @@ export default function AddBook() {
       ...state,
       [name]: value,
     });
+
+   
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-     dispatch(addBook(state))
-    console.log("The current state is",state )
-    
+    if (isEditing) {
+      dispatch(updateBook(state));
+    } else {
+      dispatch(addBook(state));
+    }
+
+    setState({});
   };
   return (
     <div className="max-w-[360px] rounded-md p-[20px] shadow-xl">
       <form onSubmit={handleSubmit}>
         <p className="text-center font-bold text-[20px]"> Add New Book </p>
         <div className="mt-[20px]">
-          <label
-            htmlFor="name"
-            className="block text-gray-600 font-bold mb-2"
-          >
+          <label htmlFor="name" className="block text-gray-600 font-bold mb-2">
             Book Name
           </label>
           <input
@@ -46,7 +69,7 @@ export default function AddBook() {
             onChange={handleChange}
             type="text"
             name="name"
-            value={name}
+            value={name ?? ""}
             id="name"
             className="px-3 py-1 border border-gray-300 rounded focus:outline-none focus:border-black-500 w-full"
           />
@@ -64,7 +87,7 @@ export default function AddBook() {
             onChange={handleChange}
             type="text"
             name="author"
-            value={author}
+            value={author ?? ""}
             id="author"
             className="px-3 py-1 border border-gray-300 rounded focus:outline-none focus:border-black-500 w-full"
           />
@@ -83,7 +106,7 @@ export default function AddBook() {
             type="text"
             name="thumbnail"
             id="thumbnail"
-            value={thumbnail}
+            value={thumbnail ?? ""}
             className="px-3 py-1 border border-gray-300 rounded focus:outline-none focus:border-black-500 w-full"
           />
         </div>
@@ -102,7 +125,7 @@ export default function AddBook() {
               type="number"
               name="price"
               id="price"
-              value={price}
+              value={price ?? ""}
               className="px-3 py-1 border border-gray-300 rounded focus:outline-none focus:border-black-500 w-full"
             />
           </div>
@@ -119,7 +142,7 @@ export default function AddBook() {
               type="number"
               name="rating"
               max={5}
-              value={rating}
+              value={rating ?? ""}
               id="rating"
               className="px-3 py-1 border border-gray-300 rounded focus:outline-none focus:border-black-500 w-full"
             />
@@ -132,7 +155,7 @@ export default function AddBook() {
             type="checkbox"
             id="featured"
             name="featured"
-            checked={featured}
+            checked={featured ?? ""}
             onChange={handleChange}
           />
           <label
@@ -149,7 +172,7 @@ export default function AddBook() {
             disabled={false}
             className=" w-full text-white text-[14px] font-bold text-lg mt-[8px] bg-orange-400 hover:bg-orange-500 active:bg-black px-[30px] py-[4px] rounded-md shadow-md cursor-pointer transition-all duration-200"
           >
-            Add Product
+            {isEditing ? "Edit Product" : "Add Product"}
           </button>
         </div>
       </form>
